af926c9c22d1562aab905acc03e1ecfb66c6edbb
chary-vk <vchary43@gmail.com>
2021-12-03 21:45:15 +0530

    changed database settings

--- functions/db.php
+++ functions/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$db = "assignments";
-$host = "localhost";
-$user = "root";
-$pass = "";
+$host = "bkalrcgh3pkvmk71zsza-mysql.services.clever-cloud.com"; //"localhost";
+$db = "bkalrcgh3pkvmk71zsza"; //"assignments"; 
+$user = "u8fhikpnddpazqci"; //"root";
+$pass = "cyxBkJtvS3pyXbJHO59o";
 
 
 $conn = mysqli_connect($host, $user, $pass, $db);
